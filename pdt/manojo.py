037830174a203ece2d40f1328e810333689f8e93
J.P. Filevich <juan.filevich@fing.edu.uy>
2023-04-03 20:28:33 -0300

    +manojo.tiene_flor

--- pdt/manojo.py
+++ pdt/manojo.py
@@ -5,11 +5,11 @@ from .carta import Carta
 
 class Manojo():
   def __init__(self, jugador:Jugador):
-    self.se_fue_al_mazo = False
-    self.cartas         = [None] * 3
-    self.tiradas        = [False] * 3
-    self.ultima_tirada  = -1
-    self.jugador        = jugador
+    self.se_fue_al_mazo :bool        = False
+    self.cartas         :list[Carta] = [None] * 3
+    self.tiradas        :list[bool]  = [False] * 3
+    self.ultima_tirada  :int         = -1
+    self.jugador        :Jugador     = jugador
   
   def __str__(self) -> str:
     cs = ", ".join([str(c) for c in self.cartas])
@@ -32,4 +32,30 @@ class Manojo():
   """
   def get_carta_idx(self, carta:Carta) -> int:
     return self.cartas.index(carta)
+  
+  """devuelve `true` si el jugador tiene flor.
+  Y ademas, si tiene devuelve que tipo de flor: 1, 2 o 3"""
+  def tiene_flor(self, muestra:Carta) -> tuple[bool, int]:
+    # caso 1: al menos dos piezas
+    piezas = [c.es_pieza(muestra) for c in self.cartas]
+    t = sum(piezas)
+    if t >= 2:
+      return (True, 1)
+
+    # caso 2: tres cartas del mismo palo
+    palos = [c.palo for c in self.cartas]
+    if palos.count(palos[0]) == len(palos):
+      return (True, 2) 
+    
+    # caso 3: una pieza y dos cartas del mismo palo
+    # Y ESAS DOS DIFERENTES DE LA PIEZA (piezaIdx)!
+    if t == 1:
+      idx = piezas.index(True)
+      if (self.cartas[0].palo == self.cartas[1].palo and idx == 2) or \
+         (self.cartas[0].palo == self.cartas[2].palo and idx == 1) or \
+         (self.cartas[1].palo == self.cartas[2].palo and idx == 0):
+        return (True, 3)
+
+    return (False, -1)
+
     
--- pdt/test_manojo.py
+++ pdt/test_manojo.py
@@ -26,3 +26,68 @@ def test_carta_str_rep():
   with pytest.raises(Exception):
     m_anna.get_carta_idx(Carta(2, "oro"))
 
+def test_tiene_flor():
+  # muestra
+  muestra = Carta(5, "copa")
+  
+  # jugadores
+  j_anna = Jugador("Anna", Equipo.AZUL)
+
+  # manojos
+  m_anna = Manojo(j_anna)
+
+  m_anna.cartas = [
+    Carta(6, "oro"),
+    Carta(10, "copa"),
+    Carta(7, "copa"),
+  ]
+  
+  assert m_anna.tiene_flor(muestra) == (False, -1), "no deberia tener flor"
+
+  m_anna.cartas = [
+    Carta(1, "copa"),
+    Carta(2, "oro"),
+    Carta(3, "basto"),
+  ]
+  
+  assert m_anna.tiene_flor(muestra) == (False, -1), "no deberia tener flor"
+
+  m_anna.cartas = [
+    Carta(4, "copa"),
+    Carta(2, "copa"),
+    Carta(3, "basto"),
+  ]
+  
+  assert m_anna.tiene_flor(muestra) == (True, 1), "deberia tener flor"
+
+  # nueva muestra
+  muestra = Carta(1, "copa")
+
+  m_anna.cartas = [
+    Carta(12, "copa"),
+    Carta(10, "copa"),
+    Carta(1, "basto"),
+  ]
+  
+  assert m_anna.tiene_flor(muestra) == (False, -1), "no deberia tener flor"
+
+  # nueva muestra
+  muestra = Carta(5, "copa")
+
+  m_anna.cartas = [
+    Carta(4, "copa"),
+    Carta(10, "espada"),
+    Carta(7, "espada"),
+  ]
+  
+  assert m_anna.tiene_flor(muestra) == (True, 3), "deberia tener flor"
+
+  m_anna.cartas = [
+    Carta(1, "oro"),
+    Carta(2, "oro"),
+    Carta(3, "oro"),
+  ]
+  
+  assert m_anna.tiene_flor(muestra) == (True, 2), "deberia tener flor"
+
+
